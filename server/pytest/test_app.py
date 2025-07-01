cb5e1dd46dd29a2b0eb740724399d6a299a9aeda
schweigenderFlugel <facundoezcastro@gmail.com>
2025-07-01 20:16:16 -0300

    feat: sqlmodel library for postgres and fixes

--- server/pytest/test_app.py
+++ server/pytest/test_app.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from fastapi import status
 
-def test_get_consults(client: TestClient):
-  response = client.get('/hello')
+def test_hello(client: TestClient):
+  response = client.get('/email')
   assert response.status_code == status.HTTP_200_OK
   assert response.headers["content-type"] == "application/json"
   assert response.json() == {
