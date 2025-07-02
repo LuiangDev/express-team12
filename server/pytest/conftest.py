8f2e5aa2454cfe9db9cfecad42d0f841651d78f7
schweigenderFlugel <facundoezcastro@gmail.com>
2025-07-02 16:11:03 -0300

    feat: clerk config

--- server/pytest/conftest.py
+++ server/pytest/conftest.py
@@ -18,7 +18,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
   DATABASE_URL,
-  # Only for SQLite database: connect_args={'check_same_thread': False},
   poolclass=StaticPool,
 )
 
